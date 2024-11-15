0a756f00ea30e3f8734216eb1c1477343c73bd3e
daplixo <aashugfx@gmail.com>
2024-11-15 19:35:45 +0000

    Made a change to program

--- Adventure-time.sh
+++ Adventure-time.sh
@@ -5,4 +5,6 @@ sleep 1
 echo "Game is still in the development phase as the developer is still learning bash and have not decided the story line of the game yet."
 sleep 1
 echo "Follow @daplixo on github and all the other socials to get in touch"
+sleep 1
+echo "Thank you"
 
